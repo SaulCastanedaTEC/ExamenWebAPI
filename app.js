ba9f3097c4db77980b2cee2662614d6a3b47e2a6
Saul Castañeda <18170381@itculiacan.edu.mx>
2023-11-20 16:31:30 -0700

    Commit 2 - Endpoint para select

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ const sqlConfig = {
 // Middleware para parsear el cuerpo de las solicitudes a JSON
 app.use(express.json());
 
+// Endpoint para obtener todos los artículos
+app.get('/articulos', async (req, res) => {
+  let pool;
+  try {
+    pool = await mssql.connect(sqlConfig);
+    const result = await pool.request().query('SELECT * FROM Publicacion');
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error en la consulta:', error.message);
+    res.status(500).send('Error en la consulta: ' + error.message);
+  } finally {
+    // Cerrar la conexión después de usarla
+    if (pool) {
+      pool.close();
+    }
+  }
+});
 
 // Inicia el servidor
 app.listen(port, () => {
